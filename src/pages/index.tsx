9a7bce1336bb17fb149b04579b12ae83c81d3143
sunya9 <mugen.xyz@gmail.com>
2021-01-14 02:15:26 +0900

    feat: add links to list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
 import Head from "next/head";
+import { GetStaticProps } from "next";
+import { promises as fs } from "fs";
+import * as path from "path";
 
 interface LinkInfo {
   title: string;
-  path: string;
+  filenameWithoutExt: string;
 }
-const linkList: LinkInfo[] = [{ title: "Sign up", path: "1-sign-up" }];
 const title = "Daily UI by sunya9";
 
-const Index = () => {
+interface Props {
+  linkInfoList: LinkInfo[];
+}
+
+const Index = (props: Props) => {
   return (
     <div className="">
       <div className="flex flex-col h-screen">
@@ -23,9 +29,11 @@ const Index = () => {
         <div className="prose max-w-none flex-1">
           <main className="mx-3 lg:mx-auto container py-10">
             <ol>
-              {linkList.map((link) => (
-                <li key={link.path}>
-                  <Link href={`/ui/${link.path}`}>{link.title}</Link>
+              {props.linkInfoList.map((link) => (
+                <li key={link.filenameWithoutExt}>
+                  <Link href={`/ui/${link.filenameWithoutExt}`}>
+                    {link.title}
+                  </Link>
                 </li>
               ))}
             </ol>
@@ -52,5 +60,58 @@ const Index = () => {
     </div>
   );
 };
+
+interface FileInfo {
+  index: number;
+  filenameWithExt: string;
+}
+
+const getConfig = () => ({
+  uiDir: path.resolve(process.cwd(), "src", "pages", "ui"),
+});
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  return fs
+    .readdir(getConfig().uiDir)
+    .then(createFileInfoListFromFilenameList)
+    .then((linkInfoList) => ({
+      props: {
+        linkInfoList,
+      },
+    }));
+};
+
+const createFileInfoListFromFilenameList = (filenames: string[]) =>
+  Promise.all(
+    filenames
+      .reduce<FileInfo[]>((memo: FileInfo[], filename: string) => {
+        const res = parseFilename(filename);
+        return res ? memo.concat(res) : memo;
+      }, [])
+      .sort((a, b) => a.index - b.index)
+      .map(createLinkInfoFromFileInfo)
+  );
+
+const filenameRegexp = /^((\d+)-.+)\.tsx$/;
+
+const parseFilename = (filename: string): FileInfo | null => {
+  const res = filename.match(filenameRegexp);
+  if (!res) return null;
+  const [, filenameWithExt, index] = res;
+  return { index: +index, filenameWithExt };
+};
+
+const titleRegexp = /<title>\s*\d+\.\s(.+)<\/title>/m;
+
+const createLinkInfoFromFileInfo = (fileInfo: FileInfo): Promise<LinkInfo> =>
+  fs
+    .readFile(
+      path.join(getConfig().uiDir, `${fileInfo.filenameWithExt}.tsx`),
+      "utf-8"
+    )
+    .then((body) => {
+      const title = body.match(titleRegexp)?.[1].trim() || "";
+      return { title, filenameWithoutExt: fileInfo.filenameWithExt };
+    });
 
 export default Index;
